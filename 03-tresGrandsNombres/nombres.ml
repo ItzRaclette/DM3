a50842e22abfbec125315bb452e4bb5d618c6c73
ItzRaclette <78966646+ItzRaclette@users.noreply.github.com>
2025-03-17 00:17:24 +0100

    Update nombres.ml

--- 03-tresGrandsNombres/nombres.ml
+++ 03-tresGrandsNombres/nombres.ml
@@ -33,19 +33,15 @@ let rec eval tree =
   match tree with
   | Zero -> 0
   | Un -> 1
-  | Node(g, p, d) -> eval g * eval p + eval d
-
-
-(* Test de eval sur les exemples de l'énoncé *)
-let () =
-  let output = List.map eval [deux; trois; cinquante; ex_818] in
-  assert (output = [2; 3; 50; 818])
+  | Node(g, p, d) -> eval g +  Suites.x (eval p) * eval d
 
 
 (** [trouve_p n] vaut la valeur de p dans le triplet (g, p, d)
     correspondant à [n] *)
 let rec interm p n =
-   if Suites.x p > n then p - 1 else interm (p+1) n
+  if p>5 then 5 else if Suites.x p > n then
+  p - 1
+  else interm (p+1) n
 
 let trouve_p n =
   if n < 2 then failwith "pas de p associe" else interm 0 n
@@ -55,16 +51,8 @@ let trouve_p n =
 let rec of_int n =
   if n = 0 then Zero else
     if n = 1 then Un else
-      let x_p = Suites.x p in
+      let x_p = Suites.x (trouve_p n) in
         Node(of_int (n mod x_p), of_int (trouve_p n), of_int (n / x_p))
-
-
-(* Test de of_int sur les exemples de l'énoncé *)
-let () =
-  let output = List.map of_int [2; 3; 50; 818] in
-  assert (output = [deux; trois; cinquante; ex_818])
-
-
 
 (***************************)
 (* Signature et générateur *)
@@ -75,44 +63,61 @@ let rec signature tree =
   match tree with
   | Zero -> 0
   | Un -> (Suites.u 10) mod 97
-  | Node(g,p,d) -> if p mod 2 = 1 then
-    ((signature g) + (Suites.u 20) * signature d) mod 97
-  else ((signature g) + (Suites.u 30) * signature d) mod 97
+  | Node(g,p,d) ->
+    let sig_g = signature g in
+    let sig_d = signature d in
+    if (eval p) mod 2 = 1 then
+    ((sig_g) + (Suites.u 20) * sig_d) mod 97
+  else ((sig_g) + (Suites.u 30) * sig_d) mod 97
 
-(***************************)
-(* Q 10 *)
-(***************************)
-let t k n = 
-  of_int (Suites.v k n)
+let rec puissance n k=
+  if k = 0 then 1
+  else n * puissance n (k-1)
 
-let a = signature (t 1 10) 
-let b = signature (t 2 20) 
-let c = signature (t 32 30) 
-let d = signature (t 61 40) 
-
-(***************************)
-(* Q 11 *)
-(***************************)
-
-let s_11 k = 
-  signature (of_int (Suites.h (Suites.v k 7*k)))
-
-let a11 =s_11 0
-let b11 =s_11 2
-let c11 =s_11 4
-let d11 =s_11 8
-
-(***************************)
-(* Q 12 *)
-(***************************)
-
-let rec gen k n =
-  if n = 0 then Zero else
-    if n = 1 then Un else
+ let sig_v k n =
+   signature (of_int(Suites.v k n))
 
 
+let rec sig_h_interm k =
+   if k < 2 then signature (of_int (Suites.h k)) else
+   let resultat = sig_h_interm (k-1) in
+   (resultat + (Suites.u 20)*resultat) mod 97
+ let sig_h k =
+   sig_h_interm (Suites.v k (7*k))
 
-  
 
-let () =
-  print_endline "All tests passed !"
+let rec sig_gen_interm k n =
+  if k=0 then
+    if Suites.u n mod 7 = 0 then signature Zero else signature Un
+  else
+    let k' = max 0 (k - 1 -((Suites.u n)mod 2)) in
+    let g = sig_gen_interm k' ((n+1)mod 997) in
+    let p = (Suites.v k' n) in
+    let d = sig_gen_interm k' ((n+2)mod 997) in
+    if d = 0
+      then g
+    else
+      let sig_p = Suites.u (if p mod 2 = 0 then 30 else 20) in
+      (g + sig_p * d) mod 97
+
+
+
+
+let sig_gen k n =
+  sig_gen_interm k n
+
+
+(*******************)
+(** Décrémentation *)
+(*******************)
+
+let sig_dec_gen k =
+  failwith "TODO"
+
+
+(*******************)
+(** Incrémentation *)
+(*******************)
+
+let sig_inc_gen k =
+  failwith "TODO"
